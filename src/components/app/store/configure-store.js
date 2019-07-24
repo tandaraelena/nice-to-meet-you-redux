b50bbd14504ccc8ff66aafa19eefaade48a604a0
tandaraelena <tandaraelena@yahoo.com>
2019-07-24 09:24:31 +0100

    redux store configuration

---
+++ src/components/app/store/configure-store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux'
+import testReducer from '../../testArea/testReducer'
+
+export const configureStore = () => {
+  const store = createStore(testReducer)
+
+  return store;
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/app/App';
+import { Provider } from 'react-redux'
+import { configureStore } from './components/app/store/configure-store';
 
-ReactDOM.render(<App />, document.getElementById('here-renders-my-app'));
+const store = configureStore()
+console.log(store.getState())
+
+const rootElem = document.getElementById('here-renders-my-app');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  rootElem);
